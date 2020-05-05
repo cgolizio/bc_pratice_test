af142c5857c77d379cb0d0b0dd7453125eea6928
Ubuntu <ubuntu@ip-172-31-84-29.ec2.internal>
2020-05-05 20:17:16 +0000

    BC_practice_test: complete

--- index.js
+++ index.js
@@ -91,40 +91,74 @@ let dogs = [
 // greeting should take three inputs: greeting, location, and time //
 // greeting should concatenate these inputs and log them to the console //
 var greeting = (greeting, location, time) => {
-    let greet = `${greeting} + ${location} + ${time}`;
+    let greet = greeting + ' ' + location + ' ' + time;
     console.log(greet);
 }; 
-
 
 
 // 2.
 // should return a count of the number of contestants //
 var contestants = function(dogArr){
-    let count = 0;
-    for(let dogs in dogA)
+        return dogArr.length;
 };
 
 
 
 // 3.
-var filterSpecies;
+// should use the native filter method to filter out all the contestants whose species isnt 'dog' //
+var filterSpecies = dogs.filter(function(dog){
+        return dog.species === 'dog';
+    });
 
 
 
 // 4. 
-var dogContestants;
+// assign a copy of the filterSpecies array to the dogContestants variable //
+// must use spread operator to do this //
+var dogContestants = [...filterSpecies];
 
 
 
 // 5. 
-var dogsWithClasses;
+// should use native map function to assign a class property to each dog object inside the dogContestants array //
+// class should be 'red' if the dog is 0-10 pounds //
+// class should be 'yellow' if the dog is 11-20 pounds //
+// class should be 'green' if the dogs weight is over 21 pounds //
+var dogsWithClasses = dogContestants.map(function(dog){
+    if(dog['weight'] < 11){
+        dog['class'] = "red";
+    }else if(dog['weight'] > 10 && dog['weight'] < 21){
+        dog['class'] = "yellow";
+    }else{
+        dog['class'] = "green";
+    }
+    return dog;
+});
     
 
 
 // 6.
-var firstInClass;
+// should combine the dog objects from the topDogs array into a single object //
+// should use recursion to do this //
+var firstInClass = function(topDogs){
+    // base case: if the topDogs array has no more length, return an empty object //
+    // ***RETURN VALUE OF BASE CASE IS LIKE SEED; ITS THE FOUNDATION THAT DATA WILL BE ADDED TO*** // 
+    if(!topDogs.length){
+        return {};
+    }
+    // recursive case //
+    return Object.assign(topDogs[0], firstInClass(topDogs.slice(1)));
+};
+
+
 
 
 
 // 7.
-var votes;
+// count all the votes that were cast //
+// include votes for non-dog species //
+// use reduce //
+var votes = dogs.reduce(function(accDog, currDog){
+    return accDog + currDog.votes;
+}, 0);
+
